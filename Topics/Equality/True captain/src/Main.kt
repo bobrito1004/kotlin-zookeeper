cd680041534d5a94c83fb9af1c6f17bb1e1fe2bb
Alex <alexlex2001@gmail.com>
2022-11-28 06:29:09 +0300

    finished

---
+++ Topics/Equality/True captain/src/Main.kt
@@ -0,0 +1,13 @@
+// you do not need to understand how it works, ignore it
+data class Pirate(val name: String)
+
+// Do not touch the lines above
+
+fun main() {
+    // fix the declaration below
+    val captain = Pirate("Hector Barbossa")
+    println(captain.name)
+    // put your code here
+
+
+}
